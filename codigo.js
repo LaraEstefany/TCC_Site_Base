5ac117a7ebdf4985bc8982209ed817ba9d4bb774
Lara Costa <lara.estefanypc@gmail.com>
2022-11-14 02:00:51 -0300

    Base de adição de produtos no carrinho js.

--- codigo.js
+++ codigo.js
@@ -128,6 +128,7 @@ function aparecer() {
 
             botao.addEventListener("click", function () {
                 carrinho.push(item)
+                adicionarAoCarrinho()
             })
 
             div.append(figure)
@@ -139,6 +140,56 @@ function aparecer() {
             produtos.append(div)
         }
         queroQueApareca = []
+    }
+}
+
+var produtosCarrinho = document.getElementById("produtosCarrinho")
+
+function adicionarAoCarrinho() {
+    debugger
+    for (let contador = 0; contador < carrinho.length; contador++) {
+        let item = carrinho[contador]
+
+        let div = document.createElement("div")
+        div.classList.add("produtoCarrinho")
+
+        let img = document.createElement("img")
+        img.classList.add("imagemCarrinho")
+        img.src = carrinho[contador].img
+
+        let texto = document.createElement("p")
+        texto.classList.add("textoCarrinho")
+        texto.innerText = carrinho[contador].nome
+
+        let h2 = document.createElement("h2")
+        h2.classList.add("textoQuantidade")
+        h2.innerText = `Qtd.`
+
+        let number = document.createElement("input")
+        number.type = "number"
+        number.classList.add("quantidade")
+        number.value = 1
+
+        let h3 = document.createElement("h3")
+        h3.classList.add("valor")
+        h3.innerText = `R$ ${carrinho[contador].preco.toFixed(2)}`
+
+        let excluir = document.createElement("input")
+        excluir.type = "button"
+        excluir.classList.add("excluir")
+        excluir.value = "❌"
+
+        excluir.addEventListener("click", function () {
+            excluirDoCarrinho()
+        })
+
+        div.append(img)
+        div.append(texto)
+        div.append(h2)
+        div.append(number)
+        div.append(h3)
+        div.append(excluir)
+        produtosCarrinho.append(div)
     }
 }
 
